36a600b8f1438d39dfc8554733aa4ec4ae11c2a4
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-06-30 10:54:47 +0100

    Tidy up

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const Koa = require('koa')
 const route = require('koa-route')
 const serve = require('koa-static')
 const app = new Koa()
-
 const RaspiCam = require('raspicam')
 
-var camera = new RaspiCam({
+const camera = new RaspiCam({
 	mode: 'photo',
 	output: './photos/snap.jpg',
 	encoding: 'jpg',
@@ -13,27 +12,19 @@ var camera = new RaspiCam({
 	timeout: 100 
 })
 
-app.use(serve(__dirname + '/photos'))
+app
+  .use(serve(__dirname + '/photos'))
+  .use(route.get('/snap', async function(ctx){
+    try {
+      const success = camera.start()
+      ctx.body = { error: undefined, success }
+    } catch (error) {
+      ctx.body = { error, success: undefined }
+    }
+  }))
+  .listen(80)
 
-app.use(route.get('/snap', async function(ctx){
-  try {
-    const result = camera.start()
-    ctx.body = { error: undefined, result }
-  } catch (error) {
-    ctx.body = {error, result: undefined }
-  }
-}))
-
-app.listen(80)
-
-camera.on('started', ( err, timestamp ) => {
-	console.log('photo started at ' + timestamp )
-})
-
-camera.on('read', ( err, timestamp, filename )  =>  {
-	console.log('photo image captured with filename: ' + filename )
-})
-
-camera.on('exit', ( timestamp )  =>  {
-	console.log('photo child process has exited at ' + timestamp )
-})
+camera
+  .on('started', ( err, timestamp ) => console.log('photo started at ' + timestamp ) )
+  .on('read', ( err, timestamp, filename ) => console.log('photo image captured with filename: ' + filename ) )
+  .on('exit', ( timestamp ) => console.log('photo child process has exited at ' + timestamp ) )
