375d5f8171035cf8e8cae75a56912f73fc03c1ea
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-06-30 01:46:37 +0100

    Use raspicam npm module

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const route = require('koa-route')
 const serve = require('koa-static')
 const app = new Koa()
 
-const Raspistill = require('node-raspistill').Raspistill
-const raspistill = new Raspistill()
+const RaspiCam = require('raspicam')
+
+var camera = new RaspiCam({
+	mode: 'photo',
+	output: '/photos/snap.jpg',
+	encoding: 'jpg',
+	timeout: 100 
+})
 
 app.use(serve('photos/'))
 
 app.use(route.get('/snap', async function(ctx){
   try {
-    const result = await raspistill.takePhoto()
-    ctx.body = {error: undefined, result}
+    const result = camera.start()
+    ctx.body = { error: undefined, result }
   } catch (error) {
-    ctx.body = {error, result: undefined}
+    ctx.body = {error, result: undefined }
   }
 }))
 
 app.listen(80)
+
+camera.on('started', ( err, timestamp ) => {
+	console.log('photo started at ' + timestamp )
+})
+
+camera.on('read', ( err, timestamp, filename )  =>  {
+	console.log('photo image captured with filename: ' + filename )
+})
+
+camera.on('exit', ( timestamp )  =>  {
+	console.log('photo child process has exited at ' + timestamp )
+})
