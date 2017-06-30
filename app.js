10665ac17a2c1baf9eb618f382d4d994af03d38b
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-06-30 11:35:21 +0100

    Tidy up

--- app.js
+++ app.js
@@ -5,41 +5,48 @@ const app = new Koa()
 const RaspiCam = require('raspicam')
 
 const camera = new RaspiCam({
-	mode: 'photo',
-	output: './photos/snap.jpg',
-	encoding: 'jpg',
+  mode: 'photo',
+  output: './photos/snap.jpg',
+  encoding: 'jpg',
   rotation: 90,
-	timeout: 100 
+  timeout: 100 
 })
 
-app
-  .use(serve(__dirname + '/photos'))
-  .use(route.get('/', ctx => {
-    ctx.body = `
-      <html>
-        <head>
-          <title>RPI Cam</title>
-        </head>
-        <body>
-          <ul style="padding: 40px;">
-            <li><a href="/snap">Take a photo</a></li>
-            <li><a href="/snap.jpg">See the photo</a></li>
-          </ul>
-        </body>
-      </html>
-    `
-  }))
-  .use(route.get('/snap', async function(ctx){
-    try {
-      const success = camera.start()
-      ctx.body = { error: undefined, success }
-    } catch (error) {
-      ctx.body = { error, success: undefined }
-    }
-  }))
-  .listen(80)
+const homePageCtrl = ctx => {
+  ctx.body = `
+    <html>
+      <head>
+        <title>RPI Cam</title>
+      </head>
+      <body>
+        <ul style="padding: 40px;">
+          <li><a href="/snap">Take a photo</a></li>
+          <li><a href="/snap.jpg">See the photo</a></li>
+        </ul>
+      </body>
+    </html>
+  `
+}
+
+const snapCtrl = async function(ctx){
+  try {
+    const success = camera.start()
+    ctx.body = { error: undefined, success }
+  } catch (error) {
+    ctx.body = { error, success: undefined }
+  }
+}
+
+const PORT = 80
 
 camera
   .on('started', ( err, timestamp ) => console.log('photo started at ' + timestamp ) )
   .on('read', ( err, timestamp, filename ) => console.log('photo image captured with filename: ' + filename ) )
   .on('exit', ( timestamp ) => console.log('photo child process has exited at ' + timestamp ) )
+
+app
+  .use(serve(__dirname + '/photos'))
+  .use(route.get('/', homePageCtrl))
+  .use(route.get('/snap', snapCtrl))
+  .listen(PORT)
+
